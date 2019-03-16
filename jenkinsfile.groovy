f09defbffaf27531ea32af8e9000045d5e0d7930
Aliya <aliyaibraimova@Aliyas-MacBook-Air.local>
2019-03-16 09:03:20 -0700

    more instructions added

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,4 +1,19 @@
 node {
-    stage("Pull git") 
+    stage("Pull git") {
        git "git@github.com:aliyaaloca/website.git"
+    } 
+    stage("Install apache") {
+       sh "ssh ec2-user@${DEVIP}"  sudo yum install httpd -y
+    }
+    stage(Start apache) {
+       sh "ssh ec2-user@${DEVIP}  sudo systemctl start httpd
+    }
+
+    stage("Copy files") {
+       sh "rsync -aP --delete index.html ec2-user@${DEVIP}:/tmp"
+    }
+
+    stage("Move files") {
+       sh "ssh ec2-user@${DEVIP}   sudo cp -f /tmp/index.html  /var/www/html/index.html    
+    } 
 }
